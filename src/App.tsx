a1a9afdd20b2c249024a4a88c2697be73c9552a6
david <obafemidavprince@gmail.com>
2024-07-29 14:08:35 +0100

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -9,46 +9,47 @@ import BlogDetails from "./component/BlogDetails";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 // import MapUi from "./component/MapUi";
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode";
-interface JwtPayload {
-  email: string;
-  email_verified: boolean;
-  exp: number;
-  family_name: string;
-  given_name: string;
-  iat: number;
-  iss: string;
-  jti: string;
-  name: string;
-  picture: string;
-  sub: string;
-}
+// import { GoogleLogin } from '@react-oauth/google';
+// import { jwtDecode } from "jwt-decode";
+// interface JwtPayload {
+//   email: string;
+//   email_verified: boolean;
+//   exp: number;
+//   family_name: string;
+//   given_name: string;
+//   iat: number;
+//   iss: string;
+//   jti: string;
+//   name: string;
+//   picture: string;
+//   sub: string;
+// }
 function App() {
   return (
-    <GoogleLogin
-  onSuccess={credentialResponse => {
-    const credentialResponsedecode = credentialResponse.credential
-  ? jwtDecode(credentialResponse.credential)
-  : null;
-    console.log(credentialResponsedecode);
+//     <GoogleLogin
+//   onSuccess={credentialResponse => {
+//     const credentialResponsedecode = credentialResponse.credential
+//   ? jwtDecode(credentialResponse.credential)
+//   : null;
+//     console.log(credentialResponsedecode);
 
-  }}
-  onError={() => {
-    console.log('Login Failed');
-  }}
-/>
-  // <div className="App">
-  //   <Header />
-  //   <Routes>
-  //       <Route path="/" element={<Home />} />
-  //       <Route path="/blog" element={<Blog />} />
-  //       <Route path="/blogdetails/:blogId" element={<BlogDetails />} />
-  //   </Routes>
-  //   <ContactBtn />
-  //   {/* <MapUi /> */}
-  //   <Footer/>
-  // </div>
+//   }}
+//   onError={() => {
+//     console.log('Login Failed');
+//   }}
+// />
+
+  <div className="App">
+    <Header />
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blogdetails/:blogId" element={<BlogDetails />} />
+    </Routes>
+    <ContactBtn />
+    {/* <MapUi /> */}
+    <Footer/>
+  </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <GoogleOAuthProvider clientId="197991698926-699l8j7jhbci2kmj7bl3s2fjup0j7m4m.apps.googleusercontent.com">
+    {/* <GoogleOAuthProvider clientId="197991698926-699l8j7jhbci2kmj7bl3s2fjup0j7m4m.apps.googleusercontent.com"> */}
       <App />
-    </GoogleOAuthProvider>
+    {/* </GoogleOAuthProvider> */}
       </BrowserRouter>
   </React.StrictMode>,
 )
